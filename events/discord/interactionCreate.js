f0cbcff9207ed7b672e2cb42f7902c1d8c1f4031
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2022-06-13 10:14:54 +0530

    Update interactionCreate.js

--- events/discord/interactionCreate.js
+++ events/discord/interactionCreate.js
@@ -1,10 +1,8 @@
 module.exports = (client, interaction) => {
 
     if (interaction.isCommand()) {
-
  
     const command = client.interactions.get(interaction.commandName);
-
 
     if (!command) return interaction.reply({
       content: "Something Went Wrong | Perhaps command not registered?",
