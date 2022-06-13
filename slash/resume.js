3cc1a8b05bdfa2a15635074c54b99c6d5e8cac43
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2022-06-13 10:19:38 +0530

    Update resume.js

--- slash/resume.js
+++ slash/resume.js
@@ -13,7 +13,6 @@ module.exports = {
 
     run: async (client, interaction) => {
 
-        // If the member doesn't have enough permissions
         if (!interaction.member.permissions.has('MANAGE_MESSAGES') && !interaction.member.roles.cache.some((r) => r.name === "Giveaways")) {
             return interaction.reply({
                 content: '<:wrong:984664520364552192> You need to have the manage messages permissions to pause giveaways.',
@@ -22,15 +21,10 @@ module.exports = {
         }
 
         const query = interaction.options.getString('giveaway');
-
-        // try to find the giveaway with prize alternatively with ID
         const giveaway =
-            // Search with giveaway prize
             client.giveawaysManager.giveaways.find((g) => g.prize === query && g.guildId === interaction.guild.id) ||
-            // Search with giveaway ID
             client.giveawaysManager.giveaways.find((g) => g.messageId === query && g.guildId === interaction.guild.id);
 
-        // If no giveaway was found
         if (!giveaway) {
             return interaction.reply({
                 content: 'Unable to find a giveaway for `' + query + '`.',
@@ -45,11 +39,8 @@ module.exports = {
             });
         }
 
-        // Edit the giveaway
         client.giveawaysManager.unpause(giveaway.messageId)
-            // Success message
             .then(() => {
-                // Success message
                 interaction.reply(`**[This giveaway](https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId})** has been successfully resumed!`);
             })
             .catch((e) => {
