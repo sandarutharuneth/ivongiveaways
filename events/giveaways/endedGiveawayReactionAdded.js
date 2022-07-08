3cb6e2b3fb566d740dbaa93c1aba5f0e40be9b71
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2022-07-08 19:33:11 +0530

    Delete events directory

--- events/giveaways/endedGiveawayReactionAdded.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js")
-module.exports = {
-  async execute(giveaway, member, reaction){
-     reaction.users.remove(member.user);
-  member.send(`**Aw snap! Looks Like that giveaway has already ended!**`).catch(e => {})
-  }
-}
