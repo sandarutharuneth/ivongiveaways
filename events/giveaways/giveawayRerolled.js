51d569d26c8f5161ce96fedea57cac819bb6b2ce
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2022-07-29 09:50:38 +0530

    Update giveawayRerolled.js

--- events/giveaways/giveawayRerolled.js
+++ events/giveaways/giveawayRerolled.js
@@ -1,13 +1,30 @@
-const Discord = require("discord.js")
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js")
 module.exports = {
   async execute(giveaway, winners) {
     winners.forEach((member) => {
       member.send({
-        embeds: [new Discord.MessageEmbed()
+        components: [new MessageActionRow()
+                    .addComponents(
+                          new MessageButton()
+                      .setLabel("Jump to the Giveaway")
+                      .setStyle("LINK")
+                      .setURL(`https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId}`)
+                      .setEmoji('973495590921043968'),
+                    new MessageButton()
+                      .setLabel("Vote Me")
+                      .setStyle("LINK")
+                      .setURL("https://discordbotlist.com/bots/ivon/upvote")
+                      .setEmoji('974160940197113916'),
+                          new MessageButton()
+                      .setLabel("Invite Me")
+                      .setStyle("LINK")
+                      .setURL("https://discord.com/api/oauth2/authorize?client_id=973436715819745290&permissions=406881561681&scope=bot%20applications.commands")
+                      .setEmoji('984296691794583582'))],
+        embeds: [new MessageEmbed()
           .setAuthor({name: "Congratulations!", iconURL: (process.env.THUMBNAIL)})
           .setThumbnail(process.env.THUMBNAIL)
           .setColor("#2F3136")
-          .setDescription(`Hello there ${member.user}\n Host of the giveaway rerolled and you have won the Giveaway on **[This Server](https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId})**\n Good Job On Winning **${giveaway.prize}!** <:confetti:973495590921043968><:confetti:973495590921043968>\nDirect Message the host to claim your prize!! \n\n [Invite](https://discord.com/api/oauth2/authorize?client_id=973436715819745290&permissions=406881561681&scope=bot%20applications.commands) | [Twitter](https://twitter.com/razerofficials) | [Patreon](https://www.patreon.com/projectrazer)`)
+          .setDescription(`Hello there ${member.user}\n Host of the giveaway rerolled and you have won the Giveaway on **[This Server](https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId})**\nGood Job On Winning **${giveaway.prize}!**\n DM the host to claim your prize!!`)
           .setTimestamp()
           .setFooter({
             text: "©️ IVON", 
