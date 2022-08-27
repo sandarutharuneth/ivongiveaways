1c65a04d3b4d78e3def677a9dc3cc961bad0378a
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2022-08-27 14:07:14 +0530

    Update serverinfo.js

--- slash/serverinfo.js
+++ slash/serverinfo.js
@@ -10,25 +10,25 @@ module.exports = {
     .setTitle("**Server Information**")
         .setColor('#2F3136')
         .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
-      .setDescription(`[Invite](https://discord.com/api/oauth2/authorize?client_id=973436715819745290&permissions=406881561681&scope=bot%20applications.commands) | [Twitter](https://twitter.com/razerofficials) | [Patreon](https://www.patreon.com/projectrazer)`)
-        .setAuthor(interaction.guild.name, interaction.guild.iconURL)
-        .addField("Name", interaction.guild.name, true)
-        .addField("Server ID", interaction.guild.id, true)
-        .addField("Owner", `${(await interaction.client.users.fetch(interaction.guild.ownerId)).tag}`, true)
-        .addField('Owner ID:', `${(await interaction.guild.ownerId)}`, true)
-        .addField(`Members`, interaction.guild.memberCount.toString(), true)
-        .addField(`Bots:`, interaction.guild.members.cache.filter(member => member.user.bot).size.toString(), true)
-        .addField(`Non-Animated Emojis:`, interaction.guild.emojis.cache.size.toString(), true)
-        .addField(`Animated Emojis:`,interaction.guild.emojis.cache.filter(emoji => emoji.animated).size.toString(),true )
-        .addField(`Text Channels:`,interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_TEXT').size.toString(),true )
-        .addField(`Voice Channels:`,interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_VOICE').size.toString(),true )
-        .addField(`Roles:`, interaction.guild.roles.cache.size.toString(), true)
-        .addField(`Created at`, `${moment(interaction.guild.createdTimestamp).format('LLL')} | \`${moment(interaction.guild.createdTimestamp).fromNow()}\``, true)
-        .addField(`Boost Level`, interaction.guild.premiumTier.toString(), true)
-        .addField(`Total Boosts`, interaction.guild.premiumSubscriptionCount.toString(), true)
+      .setDescription(`
+      **Name:** \`${interaction.guild.name}\`
+      **Server ID:** ${interaction.guild.id}
+      **Owner:** <@${(interaction.guild.ownerId)}>
+      **Owner ID:** ${(await interaction.guild.ownerId)}\n
+      **Created At: ** <t:${parseInt(interaction.guild.createdTimestamp / 1000)}:R>
+      **Members:** \`${interaction.guild.memberCount}\`
+      **Roles:** \`${interaction.guild.roles.cache.size}\`
+      **Emojis:** \`${interaction.guild.emojis.cache.size}\`
+      **Animated Emojis:** \`${interaction.guild.emojis.cache.filter(emoji => emoji.animated).size}\`
+      **Text Channels:** \`${interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_TEXT').size}\`
+      **Voice Channels:** \`${interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_VOICE').size}\`\n\n
+      **Boost Information**\n
+      **Total Boosts:** \`${interaction.guild.premiumSubscriptionCount}\`
+      **Boost Level:** \`${interaction.guild.premiumTier}\``)
+        .setAuthor({ name: interaction.guild.name })
         .setFooter({
         text: `©️ IVON`, 
-        iconURL: (process.env.FOOTERIMG)
+        iconURL: ('https://i.imgur.com/sB02Hbz.png')
     })
     interaction.reply({embeds: [embed]});
   }
