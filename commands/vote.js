7ef197b101c57837ab7cb8671598837731820da2
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2022-10-02 20:12:14 +0530

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -8,19 +8,18 @@ module.exports.run = async (client, message, args) => {
         .setLabel(`Vote on Discord Bot List`)
         .setStyle('LINK')
         .setEmoji('974160940197113916')
-        .setURL(``),//Your Bot list link
-        
-        
-        //Unfortunately you cannot add your bot on top.gg unless you make significant changes on commands or making some new advanced commands to your bot code.
-        //But you can add your bot on Discord bot list
-        
-        
+        .setURL(``),//Your Bot list link        
         new MessageButton()
         .setLabel('Invite IVON')
         .setStyle('LINK')
         .setEmoji('973537545289875486')
         .setURL(``), //Your Invite link
     )
+    
+    //Unfortunately you cannot add your bot on top.gg unless you make significant changes on commands or making some new advanced commands to your bot code.
+    //But you can add your bot on Discord bot list
+    
+    
     let invite = new MessageEmbed()
      .setAuthor({ 
           name: `${client.user.username}`, 
