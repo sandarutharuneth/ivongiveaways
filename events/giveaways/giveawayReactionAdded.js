29f9b51892158206b93807d193b1d39122e974fe
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2022-10-21 19:24:09 +0530

    Server Joining's Removed

--- events/giveaways/giveawayReactionAdded.js
+++ events/giveaways/giveawayReactionAdded.js
@@ -61,21 +61,6 @@ module.exports = {
     let client = messageReaction.message.client
     if (reactor.user.bot) return;
     if(giveaway.extraData) {
-      if (giveaway.extraData.server !== "null") {
-        try { 
-        await client.guilds.cache.get(giveaway.extraData.server).members.fetch(reactor.id)
-        return reactor.send({
-          embeds: [approved],
-          components: [noice]
-        });
-        } catch(e) {
-          messageReaction.users.remove(reactor.user);
-          return reactor.send({
-            embeds: [denied],
-            components: [lol]
-          }).catch(e => {})
-        }
-      }
       if (giveaway.extraData.role !== "null" && !reactor.roles.cache.get(giveaway.extraData.role)){ 
         messageReaction.users.remove(reactor.user);
         return reactor.send({
