c2c54277beac92f1fed2e7cc3d51077d68539690
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2023-01-06 18:38:52 +0530

    Update endedGiveawayReactionAdded.js

--- events/giveaways/endedGiveawayReactionAdded.js
+++ events/giveaways/endedGiveawayReactionAdded.js
@@ -1,7 +1,14 @@
-const Discord = require("discord.js")
+const Discord = require('discord.js');
 module.exports = {
-  async execute(giveaway, member, reaction){
-     reaction.users.remove(member.user);
-  member.send(`**Aw snap! Looks Like that giveaway has already ended!**`).catch(e => {})
-  }
-}
+  async execute(giveaway, member, reaction) {
+    reaction.users.remove(member.user);
+    member.send({
+        embeds: [
+          new Discord.EmbedBuilder()
+            .setColor("#2F3136")
+                       .setDescription("<:deprecated:1011850393334063154> **Oops! Looks Like that giveaway has already ended!** <:ended:1033984179672727553>"),
+        ],
+      })
+      .catch((e) => {});
+  },
+};
