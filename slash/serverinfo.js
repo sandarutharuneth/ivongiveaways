57df6ec87f36d57c410df073ee7be0468a7e37bd
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2023-01-06 18:35:01 +0530

    Update serverinfo.js

--- slash/serverinfo.js
+++ slash/serverinfo.js
@@ -1,30 +1,29 @@
-const Discord = require('discord.js');
-const moment = require('moment');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: 'serverinfo',
   description: 'sends the serverinfo',
   run: async (client, interaction) => {
 
-    let embed = new Discord.MessageEmbed()
+    let embed = new EmbedBuilder()
     .setTitle("**Server Information**")
         .setColor('#2F3136')
         .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
       .setDescription(`
-      **Name:** \`${interaction.guild.name}\`
-      **Server ID:** ${interaction.guild.id}
-      **Owner:** <@${(interaction.guild.ownerId)}>
-      **Owner ID:** ${(await interaction.guild.ownerId)}\n
-      **Created At: ** <t:${parseInt(interaction.guild.createdTimestamp / 1000)}:R>
-      **Members:** \`${interaction.guild.memberCount}\`
-      **Roles:** \`${interaction.guild.roles.cache.size}\`
-      **Emojis:** \`${interaction.guild.emojis.cache.size}\`
-      **Animated Emojis:** \`${interaction.guild.emojis.cache.filter(emoji => emoji.animated).size}\`
-      **Text Channels:** \`${interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_TEXT').size}\`
-      **Voice Channels:** \`${interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_VOICE').size}\`\n\n
+      <:profile:1000770439406157904> **Name:** \`${interaction.guild.name}\`
+      <:invi:1000459192555024404><:next:1000472400049209385> **Server ID:** ${interaction.guild.id}
+      <:vip:990606565344182302> **Owner:** <@${(interaction.guild.ownerId)}>
+      <:invi:1000459192555024404><:next:1000472400049209385> **Owner ID:** ${(await interaction.guild.ownerId)}\n
+      <:birthday:1000469205520502935> **Created At: ** <t:${parseInt(interaction.guild.createdTimestamp / 1000)}:R>
+      <:member:1000768159952949258> **Members:** \`${interaction.guild.memberCount}\`
+      <:role:1000449831157907556> **Roles:** \`${interaction.guild.roles.cache.size}\`
+      <:boost:990601774438166609> **Emojis:** \`${interaction.guild.emojis.cache.size}\`
+      <a:nitro:990601542463807528> **Animated Emojis:** \`${interaction.guild.emojis.cache.filter(emoji => emoji.animated).size}\`
+      <:channel:990666581493284915> **Text Channels:** \`${interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_TEXT').size}\`
+      <:voice:990666579182223431> **Voice Channels:** \`${interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_VOICE').size}\`\n\n
       **Boost Information**\n
-      **Total Boosts:** \`${interaction.guild.premiumSubscriptionCount}\`
-      **Boost Level:** \`${interaction.guild.premiumTier}\``)
+      <:boosters:1000708963626668052> **Total Boosts:** \`${interaction.guild.premiumSubscriptionCount}\`
+      <:boost:984643370511118378> **Boost Level:** \`${interaction.guild.premiumTier}\``)
         .setAuthor({ name: interaction.guild.name })
         .setFooter({
         text: `©️ IVON`, 
