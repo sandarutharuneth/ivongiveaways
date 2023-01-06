622d20c0cde4c5b0a683c19f9770628aadad474e
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2023-01-06 18:24:22 +0530

    Update about.js

--- slash/about.js
+++ slash/about.js
@@ -1,46 +1,30 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const config = require('../config.json');
+const { ApplicationCommandOptionType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
-    name: 'about',
-    description: '📑 About IVON!',
-    run: async (client, interaction) => {
+  name: 'avatar',
+  description: 'Shows User Avatar',
+  options: [{
+    name: 'user',
+    type: ApplicationCommandOptionType.User,
+    description: 'Select a user',
+    required: false,
+  }],
+  run: async (client, interaction) => {
+    const user = interaction.options.getUser('user') || interaction.user
+
     const row = new ActionRowBuilder()
     .addComponents(
         new ButtonBuilder()
-        .setLabel(`Website`)
+        .setLabel(`Avatar URL`)
         .setStyle(ButtonStyle.Link)
-        .setEmoji('1004035160376869024')
-        .setURL(`https://ivongiveaways.com`),
-        new ButtonBuilder()
-        .setLabel('Patreon')
-        .setStyle(ButtonStyle.Link)
-        .setEmoji('981525828196257813')
-        .setURL(`https://www.patreon.com/ivongiveaways`),
-        new ButtonBuilder()
-        .setLabel('Invite')
-        .setStyle(ButtonStyle.Link)
-        .setEmoji('966345633411768381')
-        .setURL(`https://discord.com/api/oauth2/authorize?client_id=973436715819745290&permissions=406881561681&scope=bot%20applications.commands`),
-    )
-    let invite = new EmbedBuilder()
-      .setAuthor({ 
-          name: `About IVON`, 
-          iconURL: client.user.displayAvatarURL() 
-      })    
-    .addFields({name: "Property Of", value: "[Project Razer](https://github.com/razerinc)", inline: true},
-               {name: "Partnered With", value: "[Astra Service](https://www.astra-services.xyz/)", inline: true},
-               {name: "Hosted On", value: "[Zap Hosting](https://zap-hosting.com/)", inline: true},)
-    .setDescription(`IVON is a Discord giveaway bot that allows you to host giveaways on your servers without the need for an extra pair of hands. Unique role requirements, server join requirements, special role points, and many more options are included to help you save time and effectively run your giveaway.`)
-    .setColor('#2F3136')
-    .setTimestamp()
-    .setThumbnail('https://i.imgur.com/sB02Hbz.png')
-    .setImage('https://i.imgur.com/1TLIl08.png')    
-    .setFooter({
-        text: `©️ IVON`, 
-        iconURL: ('https://i.imgur.com/sB02Hbz.png')
-    })
-    
-    interaction.reply({ embeds: [invite], components: [row]});
-}
+        .setURL(`${user.displayAvatarURL({ dynamic: false, format: 'png', size: 1024 })}`))
+
+    const embed = new EmbedBuilder()
+      .setTitle(`${user.username}'s Avatar`)
+      .setColor('#2F3136')
+      .setFooter({ text: `Requested by: ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true}) })
+      .setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
+    interaction.reply({embeds: [embed], components: [row]});
+
+  }
 }
