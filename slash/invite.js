8d390074b1e2ed355c6491cc971442cde91ce492
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2023-01-06 18:32:35 +0530

    Update invite.js

--- slash/invite.js
+++ slash/invite.js
@@ -1,40 +1,42 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const config = require('../config.json');
 
 module.exports = {
     name: 'invite',
-    description: '➕ Invite IVON to your server!',
+    description: '➕ Invite the bot to your server!',
     run: async (client, interaction) => {
-    const row = new MessageActionRow()
+    const row = new ActionRowBuilder()
     .addComponents(
-      new MessageButton()
+        new ButtonBuilder()
         .setLabel(`Invite ${client.user.username}`)
-        .setStyle('LINK')
-      .setEmoji('973537545289875486')
+        .setStyle(ButtonStyle.Link)
+        .setEmoji('973537545289875486')
         .setURL(`https://discord.com/api/oauth2/authorize?client_id=973436715819745290&permissions=406881561681&scope=bot%20applications.commands`),
-      new MessageButton()
+        new ButtonBuilder()
         .setLabel('Patreon')
-        .setStyle('LINK')
+        .setStyle(ButtonStyle.Link)
         .setEmoji('981525828196257813')
-        .setURL("https://www.patreon.com/projectrazer"),
-      new MessageButton()
+        .setURL("https://www.patreon.com/ivongiveaways"),
+        new ButtonBuilder()
         .setLabel('Support')
-        .setStyle('LINK')
+        .setStyle(ButtonStyle.Link)
         .setEmoji('971675354555121675')
         .setURL("https://discord.gg/7CQJvMYTrj"),
     )
-    let invite = new MessageEmbed()
+    let invite = new EmbedBuilder()
       .setAuthor({ 
           name: `Invite ${client.user.username}`, 
           iconURL: client.user.displayAvatarURL() 
       })    
     .setTitle("Invite & Support Link!")
-    .setDescription(`Invite ${client.user} to your server today and host seamless giveaways in your server!`)
+    .setDescription(`I'm The Perfect Giveaway bot to host Giveaways easily on your server with multiple options, Including role requirements, bonus roles, bonus points and server joinings. Get started with IVON today!`)
+    .setImage('https://i.imgur.com/1TLIl08.png')  
     .setColor('#2F3136')
     .setTimestamp()
-    .setThumbnail(process.env.THUMBNAIL)
+    .setThumbnail('https://i.imgur.com/sB02Hbz.png')
     .setFooter({
         text: `©️ IVON`, 
-        iconURL: (process.env.FOOTERIMG)
+        iconURL: ('https://i.imgur.com/sB02Hbz.png')
     })
     
     interaction.reply({ embeds: [invite], components: [row]});
