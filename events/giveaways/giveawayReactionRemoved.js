0b3b130ae67ff209316caa21b218261b19ad5621
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2023-01-06 18:40:02 +0530

    Update giveawayReactionRemoved.js

--- events/giveaways/giveawayReactionRemoved.js
+++ events/giveaways/giveawayReactionRemoved.js
@@ -1,23 +1,26 @@
-const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js")
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
 module.exports = {
   async execute(giveaway, member) {
     return member.send({
-      components: [new MessageActionRow()
+
+      components: [new ActionRowBuilder()
                     .addComponents(
-                          new MessageButton()
+                          new ButtonBuilder()
                       .setLabel("Jump to the Giveaway")
-                      .setStyle("LINK")
+                      .setStyle(ButtonStyle.Link)
                       .setURL(`https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId}`)
                       .setEmoji('973495590921043968'))],
-      embeds: [new MessageEmbed()
+      
+      embeds: [new EmbedBuilder()
         .setTimestamp()
         .setAuthor({name: "Reaction Removed!", iconURL: "https://i.imgur.com/Jjo00oT.png"})
         .setTitle('Did You Just Remove a Reaction From A Giveaway?')
         .setColor("#2F3136")
         .setDescription(
-          `Your entery to **${giveaway.prize}** on [This Server](https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId}) have been removed!\n This means you're removed as a valid giveaway participant.\n Think It was a mistake? **Go react again!**`
+          `<:DotPink:1002212468870877304> Your entery to **${giveaway.prize}** on [This Server](https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId}) have been removed!\n <:DotPink:1002212468870877304> This means you're removed as a valid giveaway participant.\n <:DotGreen:1002212464345239643> Think It was a mistake? **Go react again!**`
         )
-        .setFooter({ text: "©️ IVON", iconURL: (process.env.FOOTERIMG) })
+        .setFooter({ text: "©️ IVON", iconURL: ('https://i.imgur.com/sB02Hbz.png') })
       ]
     }).catch(e => {})
 
