eb544126769e2fab4c0e341b081e5cc20dca7ae5
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2023-01-08 18:45:29 +0530

    Custom Bot Status

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -14,9 +14,40 @@ module.exports = async (client) => {
 
   console.log(`[ / | Slash Command ] - ✅ Loaded all slash commands!`)
   console.log(`[STATUS] ${client.user.tag} is now online!`);
-  client.user.setPresence({
-  activities: [{ name: `Your Giveaways`, type: ActivityType.Watching }],
-  status: 'online',
-});
+  const activities = [
+    'Your Giveaways',
+    '/help',
+    'www.ivongiveaways.com'
+  ]
+
+  setInterval(() => {
+    const status = activities[Math.floor(Math.random() * activities.length)];
+    client.user.setActivity({ name: `${status}`, type: ActivityType.Watching })
+  }, 5000);
 
 };
+
+
+// Do this one if you want the streaming Status //
+
+/*
+const register = require('../../utils/slashsync');
+const { ActivityType } = require('discord.js');
+
+module.exports = async (client) => {
+  
+  await register(client, client.register_arr.map((command) => ({
+    name: command.name,
+    description: command.description,
+    options: command.options,
+    type: '1'
+  })), {
+    debug: true
+  });
+
+  console.log(`[ / | Slash Command ] - ✅ Loaded all slash commands!`)
+  console.log(`[STATUS] ${client.user.tag} is now online!`);
+  
+ client.user.setActivity({ name: `Your Giveaways`, type: ActivityType.Streaming, url: 'https://youtube.com/' })
+
+};*/
