d04209eb64d02b44fc64de0257c00d997ab14e9d
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2023-08-12 22:44:21 +0530

    Update servericon.js
    Crash handling added

--- slash/servericon.js
+++ slash/servericon.js
@@ -4,23 +4,42 @@ module.exports = {
   name: 'servericon',
   description: 'sends the Server Icon',
   run: async (client, interaction) => {
-    
-const row = new ActionRowBuilder()
-    .addComponents(
+    try {
+      const iconURL = interaction.guild.iconURL({ dynamic: true, size: 1024 });
+
+      if (!iconURL) {
+        const errorEmbed = new EmbedBuilder()
+          .setDescription(`:x:  **This server doesn't have an icon.**`)
+          .setColor('#2f3136');
+
+        return interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+      }
+        await interaction.deferReply()
+
+      const row = new ActionRowBuilder()
+	  .addComponents(
         new ButtonBuilder()
-        .setLabel(`Server Icon URL`)
-        .setStyle(ButtonStyle.Link)
-        .setURL(`${interaction.guild.iconURL()}?size=1024`))
+          .setLabel('Server Icon URL')
+          .setStyle(ButtonStyle.Link)
+          .setURL(`${iconURL}?size=1024`)
+      );
 
-    let embed = new EmbedBuilder()
-    .setAuthor({ name: interaction.guild.name })
-    .setImage(interaction.guild.iconURL({ dynamic: true, size: 1024 }))
-    .setColor('#2F3136')
-    .setFooter({
-        text: `©️ IVON`, 
-        iconURL: ('https://i.imgur.com/sB02Hbz.png')
-    })
-    interaction.reply({embeds: [embed], components: [row]});
+      const embed = new EmbedBuilder()
+        .setAuthor({ name: interaction.guild.name })
+        .setImage(iconURL)
+        .setColor('#2F3136')
+        .setFooter({
+          text: '©️ IVON',
+          iconURL: 'https://i.imgur.com/sB02Hbz.png',
+        });
 
-  }
-}
+      interaction.editReply({ embeds: [embed], components: [row] });
+    } catch (error) {
+      const errorEmbed = new EmbedBuilder()
+        .setDescription(`:x: **Something went wrong while retrieving the server icon.**`)
+        .setColor('#2f3136');
+
+      interaction.editReply({ embeds: [errorEmbed] });
+    }
+  },
+};
